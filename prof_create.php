c4229b59f95e6a56b3bc00b0610a655004a08499
jayeon <jyc975@naver.com>
2011-11-21 00:24:46 +0900

    prof_create

--- prof_create.php
+++ prof_create.php
@@ -4,50 +4,58 @@
 <div id = "form_wrapper">
 	<p>교수 정보를 입력하세요!</p>
 	<?php
-	require_once('config.php');
-	
-	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	if (isset($_POST)){
 		$name = $content = $msg = ""; // 초기화
-		//search_bar읽기
-		if (isset($_POST['school'])) $school = escape_str($_POST['school']);
-		if (isset($_POST['major'])) $major = escape_str($_POST['major']);
-		if (isset($_POST['prof_name'])) $prof_name = escape_str($_POST['prof_name']);
+		echo "Hello!";
+		//FORM 값 읽기
+		if (isset($_POST['professor_id'])) $professor_id = escape_str($_POST['professor_id']);
+		if (isset($_POST['name'])) $name = escape_str($_POST['name']);
+		if (isset($_POST['content'])) $content = escape_str($_POST['content']);
 		
-		if($school == ""){
-			$msg = "학교를 선택해 주세요.";
+		if($professor_id == ""){
+			$msg = "ID를 입력해 주세요.";
 		}
-		if($major == ""){
-			$msg = "학과를 선택해 주세요.";
-		}
-		if($prof_name == ""){
-			$msg = "교수를 선택해 주세요.";
+		if($name == ""){
+			$msg = "이름을 입력해 주세요.";
 		}
 		if($msg == ""){
 			//INSERT문 실행
-			$query = "insert into professors)(school, major, prof_name)" .
-					"value('$name','$major','$prof_name')";
+			$query ="INSERT INTO professors()". "values()";
+			$query = "INSERT INTO professor_infos(professor_id, user_id, major_id,name, photo, content)" .
+					"VALUES('$professor_id','0','1','$name','$content')";
 			if (!mysql_query($query)) {
 				echo  "<div class='error'>INSERT failed: ".mysql_error()."</div>";
 			} else {
 				// INSERT 성공
+				$query="INSERT INTO professors(id)". "values('$professor_id')";
 				$msg = "교수정보가 등록되었습니다.";
 			}
 		}
 	}
 	?>
+	<?php
+	// 메시지가 있을 경우 메시지 출력
+	if ($msg != "") 
+		echo("<div class='message'>{$msg}</div>");
+?>
 	<form action="prof_create.php" method="post">
 		<table id = "prof_form" width="100%">
 			<tr>
-				<th>학교</th>
-				<td><input type="text" name="school" size="20"></td>
+				<th>ID</th>
+				<td><input type="text" name="professor_id" size="20"></td>
 			</tr>
 			<tr>
 				<th>학과</th>
-				<td><input type="text" name="major" size="20"></td>
+				<td><input type="text" name="major_id" size="20"></td>
 			</tr>
 			<tr>
 				<th>이름</th>
-				<td><input type="text" name="prof_name" size="20"></td>
+				<td><input type="text" name="name" size="20"></td>
+			</tr>
+			<tr>
+				<th>내용 입력</th>
+				<td><textarea name="content" cols="93" row="5"></textarea>
+				</td>
 			</tr>
 		</table>
 		<input type="submit" value="INSERT">
