3270daef8d8bff5269d97dcf84e37ebc01379fa6
jayeon <jyc975@naver.com>
2011-11-29 21:01:48 +0900

    fix

--- admin.php
+++ admin.php
@@ -2,62 +2,11 @@
   require_once "upper.php";
 ?>
 <div id="form_wraaper">
-<?php
-if(isset($_POST)){
-	$univ = $major = $msg="";	//초기화
-	//FORM 값 읽기
-	if(isset($_POST['univ']))	$univ = escape_str($_POST['univ']);
-	if(isset($_POST['major']))	$major = escape_str($_POST['major']);
-	
-	if($univ == ""){
-		$msg = "학교를 입력해 주세요.";
-	}
-	if($major == ""){
-		$msg = "학과를 입력해 주세요.";
-	}
-	if($msg == ""){
-		//INSERT문 실행
-		$query="INSERT INTO universities(name)" . 
-				"VALUES('$univ');";
-		if(!mysql_query($query)){
-			echo  "<div class='error'>INSERT failed: ".mysql_error()."</div>";
-		}else{
-			// INSERT 성공
-			$query = "SELECT id FROM universities WHERE name='$univ'";
-			$result = mysql_query($query);
-			$row = mysql_fetch_array($result);
-			
-			echo  "<div class='error'>학교 입력 성공!</div>";
-			$query="INSERT INTO majors(university_id, name)".
-					"VALUES('$row[0]', '$major');";
-			if(!mysql_query($query)){
-				echo  "<div class='error'>INSERT failed: ".mysql_error()."</div>";
-			} else {
-				echo  "<div class='error'>학과 입력 성공!</div>";
-			}
-		}
-	}
-}
-?>
-<?php
-// 메시지가 있을 경우 메시지 출력
-	if ($msg != "") 
-		echo"<div class='message'>{$msg}</div>";
-?>
 	<p>관리자 페이지</p>
-	<form action="admin.php" method="post">
-		<table id="create">
-			<tr>
-				<th>학교</th>
-				<td><input type="text" name="univ" size="20"></td>
-			</tr>
-			<tr>
-				<th>학과</th>
-				<td><input type="text" name="major" size="20"></td>
-			</tr>
-		</table>
-		<input type="submit" value="INSERT">
-	</form>
+	<div id="select">
+		<p><a href="univ_create.php">학교 입력하기</a></p>
+		<p><a href="major_create.php">학과 입력하기</a></p>
+	</div>
 </div>
 <?php
   require_once "beneath.php";
---
+++ univ_create.php
@@ -0,0 +1,58 @@
+﻿<?php
+  require_once "upper.php";
+  require_once('model.php');
+?>
+<div id="form_wraaper">
+<?php
+if(isset($_POST)){
+	$univ = $msg = "";	//초기화
+	//form 값 읽기
+	if(isset($_POST['univ'])){
+		$univ = escape_str($_POST['univ']);
+	}	
+	
+	if($univ == ""){
+		$msg = "학교를 입력해 주세요.";
+	}
+	
+	if($msg == ""){
+		$query_univ = "SELECT name FROM universities WHERE name='$univ'";
+		$result_univ = mysql_query($query_univ);
+		if (!mysql_fetch_array($result_univ){
+			//INSERT문 실행
+			$query="INSERT INTO universities(name)" . 
+				"VALUES('$univ');";
+			if(!mysql_query($query)){
+				echo  "<div class='error'>INSERT failed: ".mysql_error()."</div>";
+			}else{
+			?>
+				<script type = "text/javascript"> alert ("학교 등록 완료"); </script>
+				<?php
+				echo '<meta http-equiv = "Refresh" content = "0 ; url = login.php">';
+			}
+		}else{
+			echo  "<div class='error'>이미 등록된 학교입니다</div>";
+		}
+		
+	}
+}
+?>
+<?php
+// 메시지가 있을 경우 메시지 출력
+	if ($msg != "") 
+		echo"<div class='message'>{$msg}</div>";
+?>
+	<p>학교 생성 페이지</p>
+	<form action="univ_create.php" method="post">
+		<table id="create">
+			<tr>
+				<th>이름</th>
+				<td><input type="text" name="univ" size="20"></td>
+			</tr>
+		</table>
+		<input type="submit" value="INSERT">
+	</form>
+</div>
+<?php
+  require_once "beneath.php";
+?>
