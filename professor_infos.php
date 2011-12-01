4dd5d586bed239dcda5844ffc95ae7736be7de26
Younghun Choi <choi0hun@gmail.com>
2011-12-02 02:28:36 +0900

    pagination

--- professor_infos.php
+++ professor_infos.php
@@ -5,8 +5,9 @@ require_once "upper.php";
 require_once "model.php";
 require_once "professors.php";
 $page_number = 0;
-$number_for_page = 10;
+$number_for_page = 9;
 $number_for_each_page = 10;
+
 if(isset($_GET["page"])){
   $page_number = (int)$_GET["page"];
 }
@@ -20,16 +21,36 @@ if(isset($_GET["professor_id"])){
   $infos = $infos_pagination["data"];
   $total_page = $infos_pagination["total_page"];
   $p_range = page_selection_range($page_number, $total_page, $number_for_each_page);
+  $start_index = $p_range["start_index"];
+  $last_index = $p_range["last_index"];
+  $prev_number = $p_range["prev_number"];
+  $next_number = $p_range["next_number"];
+
+
 
   
   foreach($infos as $info){
     echo <<<EOT
       <div class="history">
         <a href="professor_info.php?id={$info["id"]}">{$info["created_at"]} by {$info["user_name"]}</a>
-      </div>
-      
+      </div> 
 EOT;
   }
+    echo <<<EOT
+        <li class="prev"><a class="page_link" href="professor_infos.php?professor_id={$professor_id}&page={$prev_number}" > << </a></li>
+EOT;
+    for($i=$start_index; $i < $last_index; $i++){
+      $selected="not_selected";
+      if($i == $page_number){
+        $selected="selected";
+      }
+      echo <<<EOT
+         <li class="{$selected}"><a class="page_link" href="professor_infos.php?professor_id={$professor_id}&page={$i}" >[{$i}]</a></li>
+EOT;
+    }
+      echo <<<EOT
+         <li class="{$selected}"><a class="page_link" href="professor_infos.php?professor_id={$professor_id}&page={$next_number}" > >> </a></li>
+EOT;
 }
 
 ?>
