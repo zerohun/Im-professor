0328a2d473f672c2e5b2621a8139340851fb2e3e
Younghun Choi <choi0hun@gmail.com>
2011-12-02 18:20:11 +0900

    상단에 교수 찾아가는 메뉴 고침

--- professors_ajax.php
+++ professors_ajax.php
@@ -1,26 +1,16 @@
 <?php
   require_once("config.php");
   require_once("model.php");
+  require_once("professors.php");
   
-  if(isset($_GET)){
+  if(isset($_GET["id"])){
+
     $major_id = $_GET["id"];
-    $prof_model = new Model;
-    $prof_model->fetch("professors", array("id"));
 
-    $info_model = new Model;
-    $info_model->fetch("professor_infos", array("name"), "WHERE major_id={$major_id} ORDER BY created_at DESC LIMIT 1");
-
-    $profs = $prof_model->to_array();
-    $infos = $info_model->to_array();
-    $combined = array();
-    foreach ($profs as $p){
-      $combined["id"] = $p["id"];
-    }
-
-    foreach ($infos as $i){
-      $combined["name"] = $i["name"];
-    }
-    echo json_encode($combined);
+    $professor_model = new Professors;
+    $professor_model->find_professor_by_major_id($major_id);
+    $professors = $professor_model->to_array();
+    echo json_encode($professors);
   }
   else{
     die();
